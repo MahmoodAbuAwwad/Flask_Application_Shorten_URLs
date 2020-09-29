8c8985d9d9d1733fdadc2680b20fe588a745f42d
Mahmood Abu Awwad <abuawwadmahmood@gmail.com>
2020-09-29 16:54:36 +0300

    rendering templates

---
+++ app/__init__.py
@@ -0,0 +1,5 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__,template_folder="templates")
+
+from app import routes
--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-@app.route('/Hello')
-def Hello():
-    return "<html><body><h1>Hello, World!</h1></body></html>"
+from app import app
