74b515dc5f7ebb6cd9eec3baf5d45b5432767378
Mahmood Abu Awwad <abuawwadmahmood@gmail.com>
2020-09-29 16:07:05 +0300

    prepare and run flask server with project virtual enviroment

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hey There!")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/Hello')
+def Hello():
+    return "<html><body><h1>Hello, World!</h1></body></html>"
